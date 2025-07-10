8dc13f96d19092c6c307449a848c67f7d7b431a7
Jeremy5204 <wvjeremy5204@gmail.com>
2025-07-10 20:26:11 +0800

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,3 +1,11 @@
+export default function handler(req, res) {
+  if (req.method === 'POST') {
+    console.log('收到LINE事件:', req.body);
+    res.status(200).send('OK');
+  } else {
+    res.status(405).send('Method Not Allowed');
+  }
+}
 import { google } from 'googleapis';
 import line from '@line/bot-sdk';
 
